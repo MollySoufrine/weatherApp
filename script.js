ae43f69cfbfd898c8f40d79af5f955e95638633c
molly soufrine <mollysoufirne@gmail.com>
2020-06-25 20:49:25 -0400

    trying to pull objects from weather

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $("#search-button").on("click", function () {
     var searchValue = $("#search-value").val();
-    var list = $(".form-group").append($("<ul></ul>")).find("ul");
-    list.append("<li></li>");
-    for (var i = 0; i < searchValue.length; i++) list.text(searchValue);
-    console.log(searchValue);
+    var list = $(".list-group").append(
+      "<li class='list-group-item'>" + searchValue + "</li>"
+    );
+    console.log("this is my city " + searchValue);
 
     // clear input box
     $("#search-value").val("");
@@ -16,24 +16,24 @@ $(document).ready(function () {
     $.ajax({
       type: "GET",
       url:
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
         searchValue +
         "&appid=" +
         /* your API key here */ "2885f475462b7d71f9a31bb75ce8a58a" +
         "&units=imperial",
       dataType: "json",
       success: function (data) {
-        console.log(data);
         var data = data;
-
-        // Data is weather info back from the API
-        var weather = $("<p>");
-        weather.text(data.weather);
-
-        var main = $("<p>");
-        main.text(data.main);
+        console.log(data);
+        var currentWeather = $("<p></p>");
+        for (var i = 0; i < 5; i++) {
+          currentWeather.text(
+            data.currentWeather[i][0].main.temp,
+            data.currentWeather[i][0].weather.description
+          );
+          $(".list-group").append(currentWeather);
+        }
       },
     });
   }
-  $("#search-value .form-group").val(localStorage.getItem("search-value"));
 });
